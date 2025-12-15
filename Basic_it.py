9e2541b80aa5fe5be5d8c10d2a3be26da4bd89ce
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-15 11:41:52 +0530

    Iteration Basic

---
+++ Basic_it.py
@@ -0,0 +1,7 @@
+list1=[1,2,3,4]   #List is an iterable
+myit=iter(list1)  #Using iter() convert into iterator
+
+print(next(myit)) #calling value using next() method
+print(next(myit))
+print(next(myit))
+print(next(myit))
